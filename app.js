bce3afd0d499c2cc20acc27e238df7b250e1adfd
Jack Perrotta <jack@Jacks-MBP.home>
2017-08-07 17:10:32 -0400

    boilerplate 90% done

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+// Authentication Packages
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var MySQLStore = require('express-mysql-session')(session);
+var bcrypt = require('bcrypt');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -21,28 +29,81 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var options = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+};
+
+var sessionStore = new MySQLStore(options);
+
+app.use(session({
+    secret: 'sdfgdfgfgh',
+    resave: false,
+    store: sessionStore,
+    saveUninitialized: false,
+    //cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
 
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        console.log(username);
+        console.log(password);
+        const db = require('./db');
+
+        db.query('SELECT id, password FROM customers WHERE email = ?', [username],
+            function(err, results, fields) {
+                if (err) { done(err) };
+
+                if (results.length === 0) {
+                    done(null, false);
+                } else {
+
+                    const hash = results[0].password.toString();
+                    bcrypt.compare(password, hash, function(err, response) {
+                        if (response === true) {
+                            return done(null, { user_id: results[0].id });
+                        } else {
+                            return done(null, false);
+                        }
+                    });
+                }
+            })
+    }
+));
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
@@ -54,14 +115,14 @@ const partialsDir = __dirname + '/views/partials';
 
 const filenames = fs.readdirSync(partialsDir);
 
-filenames.forEach(function (filename) {
-  const matches = /^([^.]+).hbs$/.exec(filename);
-  if (!matches) {
-    return;
-  }
-  const name = matches[1];
-  const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
-  hbs.registerPartial(name, template);
+filenames.forEach(function(filename) {
+    const matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+        return;
+    }
+    const name = matches[1];
+    const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+    hbs.registerPartial(name, template);
 });
 
 hbs.registerHelper('json', function(context) {
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,147 @@
 var express = require('express');
 var router = express.Router();
+var expressValidator = require('express-validator');
+var passport = require('passport');
+
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Registration' });
+router.get('/', function(req, res) {
+    console.log(req.user);
+    console.log(req.isAuthenticated());
+    res.render('home', { title: 'Home' });
 });
 
-router.post('/register', function(req, res, next) {
-  res.render('index', { title: 'Registration Complete' });
+/* GET profile Page*/
+router.get('/profile', authenticationMiddleware(), function(req, res) {
+
+        res.render('profile', { title: 'Profile' });
+    });
+
+
+
+/* GET login Page*/
+router.get('/login', function(req, res) {
+    res.render('login', { title: 'Login' });
 });
+
+/* POST login, still needs error message for incorrect password */
+
+// router.post('/login', passport.authenticate(
+// 	'local', {
+// 		successRedirect: '/profile',
+// 		failureRedirect: '/login'
+// 	}));
+
+router.post('/login', function(req, res) {
+
+    req.checkBody('username', 'Email is required, please try again!').notEmpty();
+    req.checkBody('username', 'The email you entered is invalid, please try again.').isEmail();
+    req.checkBody('password', 'Password is required, please try again!').notEmpty();
+
+    //validate 
+    var errors = req.validationErrors();
+
+    if (errors) {
+
+        res.render('login', {
+            title: 'Login',
+            errors: errors
+        });
+
+    } else {
+        passport.authenticate('local', {
+            successRedirect: '/profile',
+            failureRedirect: '/login'
+
+        })(req, res);
+    }
+});
+
+/* Logout and destory cookie*/
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+});
+
+/* GET registration page*/
+
+router.get('/register', function(req, res, next) {
+    res.render('register', { title: 'Registration' });
+});
+
+/* POST registration, validate form and push to db */
+
+router.post('/register', function(req, res, next) {
+    req.checkBody('email', 'Email cannot be empty.').notEmpty();
+    // req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+    // req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+    req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+    req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
+    req.checkBody('password', 'Password must be 8 characters long.').len(8, 100);
+    // req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+    // req.checkBody('password2', 'Password must be between 8-100 characters long.').len(8, 100);
+    req.checkBody('password2', 'Passwords do not match, please try again.').equals(req.body.password);
+
+    // Additional validation to ensure username is alphanumeric with underscores and dashes
+    // req.checkBody('username', 'Username can only contain letters, numbers, or underscores.').matches(/^[A-Za-z0-9_-]+$/, 'i');
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        console.log(`errors: ${JSON.stringify(errors)}`);
+
+        res.render('register', {
+            title: 'Registration Error',
+            errors: errors
+        });
+    } else {
+
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const db = require('../db.js');
+
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            // Store hash in your password DB.
+            db.query('INSERT INTO customers (email, password) VALUES (?, ?)', [email, hash],
+                function(error, results, fields) {
+                    if (error) throw error;
+
+                    db.query('SELECT LAST_INSERT_ID() as user_id', function(error, results, fields) {
+                        if (error) throw error;
+
+                        const user_id = results[0];
+
+                        console.log(results[0]);
+                        req.login(user_id, function(err) {
+                            res.redirect('/');
+                        });
+                    });
+
+                })
+        });
+    }
+});
+
+passport.serializeUser(function(user_id, done) {
+    done(null, user_id);
+});
+
+passport.deserializeUser(function(user_id, done) {
+    done(null, user_id);
+});
+
+
+function authenticationMiddleware() {
+    return (req, res, next) => {
+        console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+        if (req.isAuthenticated()) return next();
+        res.redirect('/login')
+    }
+}
 
 module.exports = router;
